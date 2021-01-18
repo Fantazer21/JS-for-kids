0050c7697dea5235d320c47a49e1d40bb75ff593
Ilya21 <6967221@gmail.com>
2021-01-18 09:13:20 +0100

    add solution to chapter 7

--- .vscode/index.js
+++ .vscode/index.js
@@ -76,3 +76,52 @@ console.log('hello world')
 
 //     console.log(out);
 
+// Task 7
+	    // Create an array of words
+    // var words = [
+    //   "javascript",
+    //   "monkey",
+    //   "mother",
+    //   "Father"
+    // ];
+
+    // // choose a random word
+    // var word = words[Math.floor(Math.random() * words.length)];
+
+    // // Set up the answer array
+    // var answerArray = [];
+    // for (var i = 0; i < word.length; i++) {
+    //   answerArray[i] = "_";
+    // }
+
+    // var remainingLetters = word.length;
+
+    // // The game loop
+    // while (remainingLetters > 0) {
+    //   // Show the player their progress
+    //   alert(answerArray.join(" "));
+
+    //   // Get a guess from the player
+    //   var guess = prompt("Try a letter, or click Cancel.");
+    //   if (guess === null) {
+    //     // Exit the game loop
+    //     break;
+    //   } else if (guess.length !== 1) {
+    //     alert("Please enter a single letter.");
+    //   } else {
+    //     guess = guess.toLowerCase();
+    //     // Update the game state with the guess
+    //     for (var j = 0; j < word.length; j++) {
+    //       if (word[j] === guess) {
+    //         answerArray[j] = guess;
+    //         remainingLetters--;
+    //       }
+    //     }
+    //   }
+    // // The end of the game loop
+    // }
+
+    // // Show the answer and congratulate the player
+    // alert(answerArray.join(" "));
+    // alert("Victory! The answer was " + word);
+
